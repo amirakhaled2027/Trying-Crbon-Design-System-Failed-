cf34125c888545748de5321af322fe0a929ead75
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-03-11 06:17:26 +0200

    Add files via upload

---
+++ carbon-tutorial-nextjs/src/app/layout.js
@@ -0,0 +1,15 @@
+
+import './globals.scss'
+
+export const metadata = {
+  title: 'Carbon + Next13',
+  description: 'IBM Carbon Tutorial with NextJS 13',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
